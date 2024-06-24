83c612ce475d258ab293a4970d21c15eade68d38
dhanikaa <dhanikaanupama2000@gmail.com>
2024-06-24 11:07:32 +0530

    Completed all blogs and used useEffect hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(15);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleClick = () =>{
-        setName('luigi');
-        setAge(18);
+    const handleDelete = (id) =>{
+        const newBlog = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlog);
     }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data)
+            })
+    },[])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
         </div>
      );
 }
